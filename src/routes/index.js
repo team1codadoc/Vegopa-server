726e183c838a76147f3683c3b2feec527ce5c7ba
Luceta <57941164+Luceta@users.noreply.github.com>
2022-05-21 01:17:31 +0900

    Add: party router in index route

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,6 +3,7 @@ import quizRouter from "./quizRouter";
 import rootRouter from "./rootRouter";
 import foodRouter from "./foodRouter";
 import userRouter from "./userRouter";
+import partyRouter from "./partyRouter";
 
 const router = express.Router();
 
@@ -10,5 +11,6 @@ router.use("/", rootRouter);
 router.use("/quiz", quizRouter);
 router.use("/food", foodRouter);
 router.use("/user", userRouter);
+router.use("/party", partyRouter);
 
 export default router;
