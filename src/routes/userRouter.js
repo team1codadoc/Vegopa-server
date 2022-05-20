1a0fb0b25dfab934cce0f09e31a7f06ce154f9e0
Luceta <57941164+Luceta@users.noreply.github.com>
2022-05-20 23:56:31 +0900

    Add: login api to add router

--- src/routes/userRouter.js
+++ src/routes/userRouter.js
@@ -1,10 +1,16 @@
 import express from "express";
 
-import { signup, accountValid, emailValid } from "../Controller/userController";
+import {
+  signup,
+  accountValid,
+  emailValid,
+  login,
+} from "../Controller/userController";
 
 const userRouter = express.Router();
 
 userRouter.post("/signup", signup);
+userRouter.post("/login", login);
 userRouter.post("/emailValid", emailValid);
 userRouter.post("/accountValid", accountValid);
 
