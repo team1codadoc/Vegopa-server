1d2fdad36b149d9836e591d40ab7e022ad1c24e5
Luceta <57941164+Luceta@users.noreply.github.com>
2022-05-20 23:20:13 +0900

    Fix: user Router add on index

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,11 +2,13 @@ import express from "express";
 import quizRouter from "./quizRouter";
 import rootRouter from "./rootRouter";
 import foodRouter from "./foodRouter";
+import userRouter from "./userRouter";
 
 const router = express.Router();
 
 router.use("/", rootRouter);
 router.use("/quiz", quizRouter);
-router.use("/food",foodRouter);
+router.use("/food", foodRouter);
+router.use("/user", userRouter);
 
 export default router;
